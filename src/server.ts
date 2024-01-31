82eebdb13838275b9c8721cfce3c212965bf3f34
DelfinoJC <jose.carlos.csc56@gmail.com>
2024-01-30 23:25:54 -0300

    fix: getting object in correct formatting

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { serverConfig } from './config/server'
 import express from 'express'
-import router from './routes/index'
+import router from './routes'
 
 const server = express()
+server.use(express.json())
 
 server.use(router)
 
