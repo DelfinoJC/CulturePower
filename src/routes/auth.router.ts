7d42ff0f95aea63ae1bcea715607fbbfea54926a
DelfinoJC <jose.carlos.csc56@gmail.com>
2024-02-06 23:30:34 -0300

    feat: router of the authorization

---
+++ src/routes/auth.router.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import * as Yup from 'yup'
+import { User } from "../models/User";
+import { ValidationError } from "yup";
+import { compare } from "bcrypt";
+import jwt from 'jsonwebtoken';
+import {authConfig} from "../config/auth";
+
+const router = Router()
+
+router.post("/", async (req, res) => {
+    const authUser = Yup.object({
+    email: Yup.string().email().required(),
+    password: Yup.string().required().min(8)
+  })
+
+  try{
+    const { email, password } = await authUser.validate(req.body, { abortEarly: false })
+    const user = await User.findOne({email})
+    if (!user){
+      return res.send({messege: `User with email ${email} not exist`})
+    }
+
+    const passwordCompare = await compare(password, user.password)
+    if(!passwordCompare) {
+      return res.send({messege: `Password invalidate`})
+    }
+
+    user.password = ''
+
+    const token = jwt.sign({sub: user.id}, authConfig.secret, {expiresIn: 24*60*60 })
+
+    res.send({user, token})
+    
+  } catch(error) {
+    const { name, message, errors } = error as ValidationError
+      res.status(406).send({ name, message, errors })
+  }
+
+})
+
+export default router
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import userRouters from './user.routers'
+import authRouters from './auth.router'
 
 const routers = Router()
 
-routers.use('/user', userRouters)
+routers.use('/userRegister', userRouters)
+routers.use('/auth', authRouters)
 
 export default routers
