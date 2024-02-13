8d0fd5d8fa9887187c3e551479488627e3b22aae
DelfinoJC <jose.carlos.csc56@gmail.com>
2024-02-13 16:03:49 -0300

    perf: Typing request and request validation

---
+++ src/schema/productSchema.ts
@@ -0,0 +1,15 @@
+import * as Yup from 'yup'
+import { TypedRequest } from '../util/typedRequest'
+
+export const productSchema = Yup.object({
+  name: Yup.string().required('Name has required'),
+  value: Yup.number().required('Value has required'),
+  amount: Yup.number().required('Quantity has required').default(0),
+  description: Yup.string().required('Description has required'),
+  photo: Yup.string(),
+})
+
+export namespace CreateProducts {
+  export type BodyType = TypedRequest<typeof productSchema>
+  export const schema = Yup.object().shape({ body: productSchema })
+}
